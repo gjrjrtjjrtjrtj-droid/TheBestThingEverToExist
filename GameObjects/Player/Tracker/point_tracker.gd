3e68f2a096fa44d62e9d951cac60d3ac48a69a86
Me <No>
2026-02-21 17:24:39 -0500

    Stuff

---
+++ GameObjects/Player/Tracker/point_tracker.gd
@@ -0,0 +1,17 @@
+extends Node
+var score = 0;
+var frozen = false;
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_timer_timeout():
+	if frozen: 
+		return
+	score -= 1

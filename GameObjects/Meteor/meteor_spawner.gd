9c18925b0fbc4d68dfcdde957daac547018c6622
Me <No>
2026-02-21 19:27:26 -0500

    asdas

---
+++ GameObjects/Meteor/meteor_spawner.gd
@@ -0,0 +1,22 @@
+extends Node2D
+var meteor = load("res://GameObjects/Meteor/MeteorProjectile.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_timer_timeout():
+	var rng = RandomNumberGenerator.new()
+	if (rng.randi_range(1,5) >= 4):
+		return
+	print("Meteor spawned")
+	var trueMeteor = meteor.instantiate()
+	trueMeteor.velocity = Vector2(rng.randf_range(-200,200),rng.randf_range(100,1000))
+	add_child(trueMeteor)
+	
